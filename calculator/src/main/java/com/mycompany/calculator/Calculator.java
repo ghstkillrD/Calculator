5bbad995136e1f87b1309ba6354e716202e004f1
Hirantha Ranathunga <67953132+ghstkillrD@users.noreply.github.com>
2022-12-15 07:17:15 +0530

    initial commit

---
+++ calculator/src/main/java/com/mycompany/calculator/Calculator.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.calculator;
+
+/**
+ *
+ * @author USER
+ */
+public class Calculator {
+
+    public static void main(String[] args) {
+        calc cal_1 = new calc();
+        cal_1.setVisible(true);
+        
+    }
+}
