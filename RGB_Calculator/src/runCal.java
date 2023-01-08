d89f8d281bde881707c51c6103ab5e28fa7def8f
Hirantha Ranathunga <67953132+ghstkillrD@users.noreply.github.com>
2023-01-08 14:01:23 +0530

    initial commit
    Colors added

---
+++ RGB_Calculator/src/runCal.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class runCal {
+    
+    public static void main(String[] args) {
+        calBody cal_1 = new calBody();
+        cal_1.setVisible(true);
+        
+    }
+    
+}
